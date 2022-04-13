6659f443b00f11168ca95fb0bd28e9f919937632
ChimaobiWisdom <wisdomchimaobi7@gmail.com>
2022-04-13 16:05:41 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,24 +5,24 @@
  * Return:void
  */
 
- void print_alphabet_x10(void)
- {
+void print_alphabet_x10(void)
+{
 	char c;
 	int i = 0;
 
-	while(i < 10)
-	
+	while (i < 10)
+
 	{
-	
+
 		c = 'a';
-		
+
 
 		while (c <= 'z')
-		
+
 		{
 
-			_putchar(c);
-			
+			_putchar(c)
+
 			c++;
 
 
@@ -33,4 +33,4 @@
 		i++;
 
 	}
- }	
+}	
