f3489231607830541c35e32d97f240c12675ae25
ChimaobiWisdom <101463351+ChimaobiWisdom@users.noreply.github.com>
2022-04-25 19:05:38 +0100

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *rot13(char *s)
+{
+int count = 0,c;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(s + count) != '\0')
+{
+
+for (c = 0; c < 52; c++)
+{
+if (*(s + count) == alphabet[c])
+{
+*(s + count) = rot13[c];
+break;
+}
+}
+count++;
+}
+
+return (s);
+}
